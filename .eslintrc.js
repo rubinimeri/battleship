3dac1eefe0d01b7e64615ec087740b9f3760cbc7
Rubin Imeri <rubinimeri6@gmail.com>
2024-04-03 21:21:28 +0200

    Initialize project with webpack

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    'no-alert': 0,
+    'import/no-cycle': 0,
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+  },
+};
