4848bbbc4c58d712ebb3366f0d9694c96f2dac2e
Rubin Imeri <rubinimeri6@gmail.com>
2024-06-28 22:29:11 +0200

    Import Gameboard class to Player.js

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,6 +1,8 @@
-export default function Player(gameboard) {
+import Gameboard from '../Gameboard/Gameboard';
+
+export default function Player() {
   return {
-    gameboard,
+    gameboard: Gameboard(),
     turn: null,
     // sendAttack method that accepts a coordinate and the enemy gameboard
     // Sends an attack to that enemy gameboard, and changes turn property to false
