186d528a281699d72f0dffa31ca82f6b4c6d1308
Rubin Imeri <rubinimeri6@gmail.com>
2024-06-29 14:06:55 +0200

    Complete userInterface

---
+++ src/userInterface.js
@@ -0,0 +1,119 @@
+import Player from './Player/Player';
+
+const UI = (() => {
+  function checkAttacks(missedAttacks, successfulAttacks, coordinates) {
+    const [row, column] = coordinates;
+    if (missedAttacks.find((attack) => row === attack[0] && column === attack[1])) return false;
+    if (
+      successfulAttacks.find((attack) => row === attack[0] && column === attack[1])) return true;
+    return null;
+  }
+
+  function loadPlayerBoard(player) {
+    const DOMBoard = document.querySelector('.player');
+    const boardLength = 10;
+
+    // Remove previous board
+    while (DOMBoard.lastChild) {
+      DOMBoard.removeChild(DOMBoard.lastChild);
+    }
+
+    const { missedAttacks, successfulAttacks } = player.gameboard;
+
+    for (let i = 0; i < boardLength; i++) {
+      // Create row element and add it to board
+      const row = document.createElement('div');
+      row.classList.add('row');
+      DOMBoard.append(row);
+
+      for (let j = 0; j < boardLength; j++) {
+        // Create cell and add it to row
+        const cell = document.createElement('div');
+        cell.dataset.x = i;
+        cell.dataset.y = j;
+        cell.classList.add('cell');
+        row.append(cell);
+
+        // Check if an attack has occured on that cell
+        if (checkAttacks(missedAttacks, successfulAttacks, [i, j])) {
+          cell.classList.add('successful-attack');
+        } else if (checkAttacks(missedAttacks, successfulAttacks, [i, j]) === false) {
+          cell.classList.add('missed-attack');
+        } else if (checkAttacks(missedAttacks, successfulAttacks, [i, j]) === null) {
+          // Check if there is a ship on that cell
+          const { grid } = player.gameboard;
+
+          if (grid[i][j].ship !== null) cell.classList.add('ship');
+        }
+      }
+    }
+  }
+  function loadComputerBoard(computer) {
+    const DOMBoard = document.querySelector('.computer');
+    const boardLength = 10;
+
+    // Remove previous board
+    while (DOMBoard.lastChild) {
+      DOMBoard.removeChild(DOMBoard.lastChild);
+    }
+
+    const { missedAttacks, successfulAttacks } = computer.gameboard;
+
+    for (let i = 0; i < boardLength; i++) {
+      // Create row element and add it to board
+      const row = document.createElement('div');
+      row.classList.add('row');
+      DOMBoard.append(row);
+
+      for (let j = 0; j < boardLength; j++) {
+        // Create cell and add it to row
+        const cell = document.createElement('div');
+        cell.dataset.x = i;
+        cell.dataset.y = j;
+        cell.classList.add('cell');
+        row.append(cell);
+
+        // Check if an attack has occured on that cell
+        if (checkAttacks(missedAttacks, successfulAttacks, [i, j])) {
+          cell.classList.add('successful-attack');
+        } else if (checkAttacks(missedAttacks, successfulAttacks, [i, j]) === false) {
+          cell.classList.add('missed-attack');
+        }
+      }
+    }
+  }
+  function startGame() {
+    const startButton = document.getElementById('start');
+    startButton.style.display = 'none';
+  }
+
+  return {
+    loadPlayerBoard,
+    loadComputerBoard,
+    startGame,
+  };
+})();
+
+const player = Player();
+const { gameboard } = player;
+
+const computer = Player();
+const computerGameboard = computer.gameboard;
+
+gameboard.placeShip([0, 0], 5);
+gameboard.placeShip([2, 0], 4);
+gameboard.placeShip([5, 5], 3);
+gameboard.placeShip([3, 1], 2);
+gameboard.placeShip([9, 7], 1);
+
+computerGameboard.placeShip([0, 0], 5);
+
+computerGameboard.receiveAttack([0, 0]);
+computerGameboard.receiveAttack([1, 0]);
+
+UI.loadPlayerBoard(player);
+UI.loadComputerBoard(computer);
+
+gameboard.receiveAttack([0, 0]);
+
+UI.loadPlayerBoard(player);
